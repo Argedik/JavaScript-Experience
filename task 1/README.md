b987f1466a7f34d40f90a1cb868a5590e0e2e118
Enes Yasin Gedik <72869669+Argedik@users.noreply.github.com>
2022-04-09 17:58:53 +0300

    ödev 1

---
+++ task 1/README.md
@@ -0,0 +1,5 @@
+# Javascript-experience
+Javascript ödevleri
+
+[Kodluyoruz](https://www.kodluyoruz.org/)  
+[Patika](https://www.patika.dev/)
---
+++ task 1/main.js
@@ -0,0 +1,19 @@
+let fullName = prompt("Adınız Nedir?")
+
+
+function dijitalSaat(){
+    var now = new Date()
+    var hour = now.getHours()
+    var minute = now.getMinutes()
+    var second = now.getSeconds()
+
+    var days = ["Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi","Pazar"]
+    var toDay = days[now.getDay()-1]
+    var time = hour + ":" + minute + ":" + second +" / "+ toDay
+    var hour = document.getElementById("myClock").innerHTML=time
+    var user = document.getElementById("myName").innerHTML = fullName
+   
+    return hour,user
+}
+
+setInterval(dijitalSaat, 1000)
